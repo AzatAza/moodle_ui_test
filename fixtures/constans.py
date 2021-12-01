9f8f8dfbc75d6be4ee79a51c61c2c533bbd595cc
AzatAza <ai.allagulov@yandex.ru>
2021-12-01 13:05:38 +0600

    UPDATE: add CICD

--- fixtures/constans.py
+++ fixtures/constans.py
@@ -1,4 +1,4 @@
 class Constants:
-    LOGIN_ERROR_MESSAGE = 'Неверный логин или пароль, попробуйте заново'
+    LOGIN_ERROR_MESSAGE = 'Неверный логин или пароль, попробуйте заново.'
     FIRSTNAME_ERROR_MESSAGE = '- Заполните поле'
 
