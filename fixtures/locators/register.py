5358e2ee0d6764d4f9897bec6a53550505c8f41f
AzatAza <ai.allagulov@yandex.ru>
2021-12-07 20:10:05 +0600

    UPDATE: add HW Decision

--- fixtures/locators/register.py
+++ fixtures/locators/register.py
@@ -17,9 +17,11 @@ class RegisterLocators():
     ERROR_EMAIL2 = (By.ID, "id_error_email2")
     ERROR_FIRSTNAME = (By.ID, "id_error_firstname")
     ERROR_LASTNAME = (By.ID, "id_error_lastname")
-    PASSWORD_LESS_THAN_8 = (By.XPATH, "//*[contains(text(), 'Passwords must be at least 8 characters long.')]")
-    PASSWORD_NOT_A_DIGIT = (By.XPATH, "//*[contains(text(), 'Passwords must have at least 1 digit(s).')]")
-    PASSWORD_NOT_LOWERCASE = (By.XPATH, "//*[contains(text(), 'Passwords must have at least 1 lower case letter(s).')]")
-    PASSWORD_NOT_UPPERCASE = (By.XPATH, "//*[contains(text(), 'Passwords must have at least 1 upper case letter(s).')]")
-    PASSWORD_NOT_A_SPEC_SYMBOL = (By.XPATH, "//*[contains(text(), 'Passwords must have at least 1 non-alphanumeric "
-                                            "character(s) such as as *, -, or #.')]")
+    PASSWORD_LESS_THAN_8 = (By.XPATH, "//*[contains(text(), 'Пароль не должен состоять из менее чем 8 символов(а).')]")
+    PASSWORD_NOT_A_DIGIT = (By.XPATH, "//*[contains(text(), 'Пароль должен содержать не менее 1 цифр(ы).')]")
+    PASSWORD_NOT_LOWERCASE = (By.XPATH, "//*[contains(text(), 'Пароль должен содержать "
+                                        "не менее 1 строчных(ой) букв(ы).')]")
+    PASSWORD_NOT_UPPERCASE = (By.XPATH, "//*[contains(text(), 'Пароль должен содержать "
+                                        "не менее 1 прописных(ой) букв(ы).')]")
+    PASSWORD_NOT_A_SPEC_SYMBOL = (By.XPATH, "//*[contains(text(), 'Пароль должен содержать не менее 1 символов, "
+                                            "не являющихся буквами и цифрами, например таких как *, - или #.')]")
